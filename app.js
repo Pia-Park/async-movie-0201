bf111f8202fffa2836d0266bb579e9a944b4409f
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-02-01 12:37:55 -0800

    added get movies

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
 // const main = get DOM element of main
+const main = document.querySelector('#main');
 // const form = get DOM element of form
+const form = document.querySelector('#form');
 // const search = get DOM element of search
+const search = document.querySelector('#search');
 
 // Get initial movies
 getMovies(API_URL)
@@ -12,6 +15,13 @@ getMovies(API_URL)
 //Create an async function of getMovies 
 //------assign a response to a fetch/axios method
 //------call showMovies function and pass the response as argument
+async function getMovies(url){
+    const response = await fetch(url)
+    const { results } = await response.json();
+    // showMovies(response);
+    console.log(results);
+
+}
 
 function showMovies(movies) {
     main.innerHTML = ''
@@ -19,15 +29,20 @@ function showMovies(movies) {
     movies.forEach((movie) => {
         //destructure the movie object
         // const { ???, ??? ,vote_average } = movie //include the "vote_average"
+        const {original_title, overview, vote_average} = movie;
 
         //create a div element
         //add a "movie" class in that div 
+    
+        movie.innerHTML = document.createElement('div');
+        movie.innerHTML.setAttribute('class', 'movie');
+        main.appendChild(movie);
 
         //manipulate the newly created element's innerHTML (I called it movieEl in this sample)
         movieEl.innerHTML = `
-            <img src="" alt="">
+            <img src="${backdrop_path}" alt="movie image">
             <div class="movie-info">
-          <h3></h3>
+          <h3 class="${original_title}">${original_title}</h3>
           <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
@@ -36,7 +51,39 @@ function showMovies(movies) {
         </div>
         `
         //append the movieEl to main
-    })
+        main.appendChild(movieEl);
+    });
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        //destructure the movie object
+        // const { ???, ??? ,vote_average } = movie //include the "vote_average"
+        const {original_title, overview, vote_average} = movie;
+
+        //create a div element
+        //add a "movie" class in that div 
+        
+        const movieEl = document.createElement('div');
+        movieEl.setAttribute('class', 'movie');
+
+        //manipulate the newly created element's innerHTML (I called it movieEl in this sample)
+        movieEl.innerHTML = `
+            <img src="${backdrop_path}" alt="movie image">
+            <div class="movie-info">
+          <h3 class="${original_title}">${original_title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+        //append the movieEl to main
+        main.appendChild(movieEl);
+    });
 }
 
 function getClassByRate(vote) {
@@ -53,10 +100,11 @@ form.addEventListener('submit', (e) => {
     //prevent refresh
 
     //const searchTerm = ???  //assign the value of the input's value
+    const searchTerm = document.querySelector('submit').value;
 
     if(searchTerm && searchTerm !== '') {
-        getMovies(SEARCH_API + searchTerm)
         //call the getMovies function and pass the concatenated value of SEARCH_API + searchTerm
+        getMovies(SEARCH_API + searchTerm)
 
         //reset the input's value
     } else {
